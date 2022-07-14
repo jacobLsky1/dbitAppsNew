aca3623e09d89cbac9fd9487fd9b16d0eed55a47
jacob lipsky <jacob.lipsky1@gmail.com>
2022-07-14 11:54:19 +0300

    with exe prints

--- dbitAppsUserScript.py
+++ dbitAppsUserScript.py
@@ -1,3 +1,5 @@
+import traceback
+
 import psycopg2
 from psycopg2 import sql
 from bs4 import BeautifulSoup
@@ -7,11 +9,12 @@ import requests
 import json
 import certifi
 
-
 cerfi = certifi.where()
 # postgresql
-postgresqlConnection = psycopg2.connect(user="dibit", password="AVNS_nmcfJN5ksfH5HPub9ph", database="dbit_apps", host="db-postgresql-dbit-apps-do-user-2230473-0.b.db.ondigitalocean.com",
+postgresqlConnection = psycopg2.connect(user="dibit", password="AVNS_nmcfJN5ksfH5HPub9ph", database="dbit_apps",
+                                        host="db-postgresql-dbit-apps-do-user-2230473-0.b.db.ondigitalocean.com",
                                         port="25060")
+# postgresqlConnection = psycopg2.connect(user="dibit", password="jgme4350jldbit", database="dbit_apps", host="localhost",port="5432")
 sqlCursor = postgresqlConnection.cursor()
 postgreSQL_select_Query = "SELECT * FROM app_primary_table"
 sqlCursor.execute(postgreSQL_select_Query)
@@ -22,13 +25,14 @@ for row in all_apps_records:
     appServer = row[2]
     appLogger = row[3]
     appSummeryId = row[4]
-    appListId = row[5] 
-    appUserListSize = row[6] 
+    appListId = row[5]
+    appUserListSize = row[6]
 
     if appLogger != "null":
 
         name = appName + "_list_id"
-        columns = (("date", "VARCHAR(255)"),("time", "VARCHAR(255)"), ("valid_status", "VARCHAR(255)"), ("device_type", "VARCHAR(255)"),
+        columns = (("date", "VARCHAR(255)"), ("time", "VARCHAR(255)"), ("valid_status", "VARCHAR(255)"),
+                   ("device_type", "VARCHAR(255)"),
                    ("country_code", "VARCHAR(255)"), ("region_name", "VARCHAR(255)"), ("ip_address", "VARCHAR(255)"),
                    ("user_agent", "VARCHAR(255)"))
         fields = []
@@ -40,13 +44,18 @@ for row in all_apps_records:
             fields=sql.SQL(', ').join(fields)
         )
         sqlCursor.execute(query)
-        urlPage = requests.get(appLogger)
-        soup = BeautifulSoup(urlPage.text, 'html.parser')
-        myString = str(soup)
-        myList = myString.split('%')
-        myList.pop(0)
-        jsonList = myList[appUserListSize:]
-        num = 0
+        try:
+            urlPage = requests.get(appLogger)
+            soup = BeautifulSoup(urlPage.text, 'html.parser')
+            myString = str(soup)
+            myList = myString.split('%')
+            myList.pop(0)
+            jsonList = myList[appUserListSize:]
+            num = 0
+        except Exception:
+            print(traceback.format_exc())
+
+
         for item in jsonList:
             json_object = json.loads(item)
             try:
@@ -82,20 +91,24 @@ for row in all_apps_records:
                 date = datetimeTemp[0]
                 time = datetimeTemp[1]
 
-            sqlCursor.execute(sql.SQL(
-                "INSERT INTO {tbl_name} (date, time, valid_status, device_type, country_code, region_name, ip_address, user_agent) values (%s, %s, %s,%s, %s,%s, %s, %s)"
-            ).format(tbl_name=sql.Identifier(name)),[date, time, status, devicetype, country_code, region_name, myip, useragent])
+            try:
+                sqlCursor.execute(sql.SQL(
+                    "INSERT INTO {tbl_name} (date, time, valid_status, device_type, country_code, region_name, ip_address, user_agent) values (%s, %s, %s,%s, %s,%s, %s, %s)"
+                ).format(tbl_name=sql.Identifier(name)),
+                                  [date, time, status, devicetype, country_code, region_name, myip, useragent])
+
+            except Exception:
+                print(traceback.format_exc())
+
             num = num + 1
             print(f"{name} - {num} out of {len(jsonList)}")
 
-
         print(f"added {num} data items to {appName + '_list_id'} that has {appUserListSize} data items")
         if num != 0:
-          newquery = sql.SQL("UPDATE app_primary_table SET user_list_size = %s WHERE app_name = %s")
-          sqlCursor.execute(newquery,[num+appUserListSize, appName])
-          print(f"set {appName} user size to {num+appUserListSize}")
+            newquery = sql.SQL("UPDATE app_primary_table SET user_list_size = %s WHERE app_name = %s")
+            sqlCursor.execute(newquery, [num + appUserListSize, appName])
+            print(f"set {appName} user size to {num + appUserListSize}")
         print("")
-
 
 postgresqlConnection.commit()
 sqlCursor.close()
